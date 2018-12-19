cd61b839a6b665cad76b7db07d00c95072a1ff7a
李祖明 <solace.lee@outlook.com>
2018-12-19 17:28:54 +0800

    exam-list

--- components/header/index.js
+++ components/header/index.js
@@ -4,20 +4,50 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    userType: {
+      type: Number,
+      value: 0
+    },
+    userName: {
+      type: String,
+      value: '用户名'
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    showLogin: true
   },
 
+  attached: function (event) {
+    this.isLogin()
+  },
   /**
    * 组件的方法列表
    */
   methods: {
+    isLogin(event) {
+      if (this.properties.userType == 0) {
+        this.setData({
+          showLogin: true
+        })
+      } else {
+        this.setData({
+          showLogin: false
+        })
+      }
+    },
+
+    goLoginIn(event) {
+      console.log('登录功能');
+    },
+
+    goLoginOut(event){
+      console.log('登出功能');
+      
+    }
 
   }
 })
--- pages/exam/exam.js
+++ pages/exam/exam.js
@@ -5,15 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    examList: [{
+      examName: "第一次月考",
+      sum: "510",
+      schoolRank: "44",
+      max: "555",
+      Average: "500",
+      min: "320"
+    }, {
+      examName: "第二次月考",
+      sum: "25",
+      schoolRank: "46",
+      max: "575",
+      Average: "540",
+      min: "420"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
